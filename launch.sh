f68e9470cc2a4ccbb2fb1889b3776df98676e744
Alexandre Fresnais <alexandre.fresnais@epita.fr>
2021-03-28 19:07:29 +0200

    [ADD] working directory

--- launch.sh
+++ launch.sh
@@ -9,7 +9,12 @@ if [ ! $? -eq 0 ]; then
         exit 1
     fi
 
-    docker run --rm -v $PWD:/pwd --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name ctf -i ctf:ubuntu
+    docker run \
+            --rm -v $PWD:/pwd \
+            --cap-add=SYS_PTRACE \
+            --security-opt seccomp=unconfined \
+            -w /home \
+            -d --name ctf -i ctf:ubuntu
     if [ ! $? -eq 0 ]; then
         exit 1
     fi
